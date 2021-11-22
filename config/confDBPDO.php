fbea2ea52544ca69fa6882cd3baf20d21857d66f
Rodrigo <Rodrigo.gerzur@educa.jcyl.es>
2021-11-22 10:13:08 +0100

    22/11/2021 mas cambios

--- config/confDBPDO.php
+++ config/confDBPDO.php
@@ -6,6 +6,10 @@
  * and open the template in the editor.
  */
 
-define("HOST","mysql:host=DAW203.sauces.local;dbname=DB203DWESProyectoTema4");
+define("HOST","mysql:host=daw203.sauces.local;dbname=DB203DWESProyectoTema4");
 define("PASSWORD", "P@ssw0rd");
 define("USER","User203DWESProyectoTema4");
+
+   //define("HOST","mysql:host=db5005797349.hosting-data.io;dbname=dbs4868791");
+   //define("PASSWORD", "paso");
+   //define("USER","dbu2267458");
