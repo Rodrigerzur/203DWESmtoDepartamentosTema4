9522ff57a8b3b08b0ddaa08377ccc8d7d926f693
Rodrigerzur <63009008+Rodrigerzur@users.noreply.github.com>
2021-11-23 20:12:06 +0100

    Update confDBPDO.php

--- config/confDBPDO.php
+++ config/confDBPDO.php
@@ -6,10 +6,10 @@
  * and open the template in the editor.
  */
 
-define("HOST","mysql:host=daw203.sauces.local;dbname=DB203DWESProyectoTema4");
-define("PASSWORD", "P@ssw0rd");
-define("USER","User203DWESProyectoTema4");
+//define("HOST","mysql:host=daw203.sauces.local;dbname=DB203DWESProyectoTema4");
+//define("PASSWORD", "P@ssw0rd");
+//define("USER","User203DWESProyectoTema4");
 
-   //define("HOST","mysql:host=db5005797349.hosting-data.io;dbname=dbs4868791");
-   //define("PASSWORD", "daw2_Sauces");
-   //define("USER","dbu2267458");
+   define("HOST","mysql:host=db5005797349.hosting-data.io;dbname=dbs4868791");
+   define("PASSWORD", "daw2_Sauces");
+   define("USER","dbu2267458");
